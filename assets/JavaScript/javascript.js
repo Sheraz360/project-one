9a8ffba8dab761cb0f8932135cacfc8106caf43e
Sheraz360 <sheraz.s.khan92@gmail.com>
2023-05-14 02:08:23 +0100

    update modal

--- assets/JavaScript/javascript.js
+++ assets/JavaScript/javascript.js
@@ -53,23 +53,64 @@ function addNewRow(event) {
 }
 
 function deleteRow(event) {
-    // Get the parent row element
     var row = event.target.closest('.topic-row');
 
-    // Remove the row from the DOM
     row.remove();
 }
 
+// Search button
 document.addEventListener('DOMContentLoaded', function () {
     // Get the search button and register a click event listener
     var searchButton = document.querySelector('.search-button');
     searchButton.addEventListener('click', function (event) {
-      event.preventDefault(); // Prevent the form submission
-  
-      // Open the modal manually
-      var modal = document.getElementById('exampleModalCenter');
-      var modalInstance = new bootstrap.Modal(modal);
-      modalInstance.show();
+        event.preventDefault(); // Prevent the form submission
+
+        // Open the modal manually
+        var modal = document.getElementById('exampleModalCenter');
+        var modalInstance = new bootstrap.Modal(modal);
+        modalInstance.show();
     });
-  });
-  
+});
+
+
+// Modal arrow buttons
+document.addEventListener('DOMContentLoaded', function () {
+    var images = document.querySelectorAll('.image-container img');
+    var currentIndex = 0;
+
+    function showImage(index) {
+        images.forEach(function (img) {
+            img.classList.remove('active');
+        });
+
+        images[index].classList.add('active');
+    }
+
+    function navigate(direction) {
+        currentIndex += direction;
+
+        if (currentIndex < 0) {
+            currentIndex = images.length - 1;
+        } else if (currentIndex >= images.length) {
+            currentIndex = 0;
+        }
+
+        showImage(currentIndex);
+    }
+
+    var leftArrow = document.querySelector('.left-arrow');
+    var rightArrow = document.querySelector('.right-arrow');
+
+    leftArrow.addEventListener('click', function (event) {
+        event.preventDefault();
+        navigate(-1);
+    });
+
+    rightArrow.addEventListener('click', function (event) {
+        event.preventDefault();
+        navigate(1);
+    });
+
+    showImage(currentIndex);
+});
+
